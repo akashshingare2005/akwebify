feed4d2869ec49937b1ad55a145c19fd185b8f97
akashshingare2005 <akashshingare217@gmail.com>
2026-01-31 19:47:28 +0530

    fix mobile navbar and responsiveness

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -13,3 +13,9 @@ document.addEventListener("DOMContentLoaded",()=>{
   });
 });
 
+const toggleBtn = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+toggleBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
